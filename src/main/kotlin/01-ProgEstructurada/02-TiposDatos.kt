20479a8b3705b31a36dceddb71df1c30c43d5009
José Luis González <joseluisgonsan@gmail.com>
2022-09-12 13:38:16 +0200

    Tipos de Datos

---
+++ src/main/kotlin/01-ProgEstructurada/02-TiposDatos.kt
@@ -0,0 +1,23 @@
+package `01-ProgEstructurada`
+
+/*
+En ciencias de la computación, un tipo de dato informático o simplemente tipo,
+es un atributo de los datos que indica al ordenador (y/o al programador/programadora)
+sobre la clase de datos que se va a manejar. Esto incluye imponer restricciones en los datos,
+como qué valores pueden tomar y qué operaciones se pueden realizar.
+
+https://kotlinlang.org/docs/basic-types.html
+
+ */
+
+fun main() {
+    val entero: Int = 10
+    val edad = 23
+    val byte: Byte = 127
+    val long = 1234567890123456789L
+    val decimal: Double = 10.5
+    val float: Float = 10.5f
+    val texto: String = "Hola mundo"
+    val booleano: Boolean = true
+    val caracter: Char = 'a'
+}
