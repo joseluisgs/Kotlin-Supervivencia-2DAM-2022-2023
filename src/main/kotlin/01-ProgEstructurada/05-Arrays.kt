b30b876d7cc94e395df26748ee1b82ff84efa2ac
José Luis González <joseluisgonsan@gmail.com>
2022-09-12 14:13:03 +0200

    05-Arrays

---
+++ src/main/kotlin/01-ProgEstructurada/05-Arrays.kt
@@ -0,0 +1,23 @@
+package `01-ProgEstructurada`
+
+/**
+ * Es una estructura de datos donde se accede por indice
+ * https://kotlinlang.org/docs/arrays.html#string-templates
+ */
+
+fun main() {
+    // Array unidimensional
+    val array1 = arrayOf(1, 2, 3, 4, 5)
+    val array2 = IntArray(10)
+    println(array2)
+    val array3 = IntArray(10) { i -> i * 2 }
+    println(array3)
+
+    // Array multidimensional
+    val array4 = Array(3) { IntArray(3) }
+    println(array4)
+    val array5 = Array(3) { IntArray(3) { i -> i * 2 } }
+    println(array5)
+    println(array1[3])
+    println(array5[2][1])
+}
