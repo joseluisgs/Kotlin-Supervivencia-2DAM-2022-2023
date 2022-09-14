e9d4d359f833943e2f8789becff13751d6d44df1
José Luis González <joseluisgonsan@gmail.com>
2022-09-14 17:51:49 +0200

    09-ScopeFunctions.kt

--- src/main/kotlin/03-ProgFuncional/09-ScopeFunctions.kt
+++ src/main/kotlin/03-ProgFuncional/09-ScopeFunctions.kt
@@ -20,3 +20,90 @@ data class Persona(
         println("Hola, me llamo $nombre y tengo $edad años")
     }
 }
+
+fun main() {
+    // Run
+    // nos permite inicializar y ejecutar un método del objeto, el propio objeto es el receptor
+    val p1 = Persona()
+    p1.run {
+        nombre = "Juan"
+        edad = 25
+        saludar()
+    }
+
+    // With, nos permite cambiar el estado de un objeto, hay que indicarle el objeto
+    val p2 = Persona()
+    with(p2) {
+        nombre = "Juan"
+        edad = 25
+        //saludar()
+    }
+    p2.saludar()
+
+    // Apply nos permite cambiar el estado de un objeto, el propio objeto es el receptor: ¡Anda la interfaz fluida!
+    val p3 = Persona()
+    p3.apply {
+        nombre = "Juan"
+        edad = 25
+    }.saludar()
+
+    // Let se suele usar para comparaciones y realizar una accion, el propio objeto es el receptor (adios if!!)
+    val p4 = Persona()
+    p4.let {
+        it.saludar()
+    }
+
+    var p5: Persona? = null
+    p5?.let {
+        it.nombre = "Juan"
+        it.saludar()
+    }
+
+    // Also se usa para realizar una accion, posterior
+    var p6 = Persona()
+    p2.apply {
+        nombre = "Juan"
+        edad = 25
+    }.also {
+        it.saludar()
+    }
+
+    val randomNull: String? = null
+    // Por favor ya no!!! que no estais en Java
+    if (randomNull != null) {
+        println(randomNull.length)
+    }
+
+    // Usando Scope Functions incluso puedo simular un if else
+    randomNull?.let {
+        println(it.length)
+    }.also { println("Hola") }
+
+    // Usando Elvis, pero e implica excribir una opcion o no o llamadas seguras
+    println(randomNull?.length ?: "No hay nada")
+    println(randomNull?.length)
+
+    val b: String? = "Me gusta Kotlin"
+    b?.let {
+        println("B no es null")
+    } ?: run {
+        println("B sí es null")
+    }.also {
+        println("ademas si hace el run hago esto")
+    }
+
+    // queda mas claro con un if
+    if (b != null) {
+        println("B no es null")
+    } else {
+        println("B sí es null")
+    }
+
+    // Swap con also
+    var var1 = 1
+    var var2 = 2
+    var1 = var2.also { var2 = var1 } // var1 = var2 y ademas var2 = var1
+    println("var1 = $var1, var2 = $var2")
+
+
+}
